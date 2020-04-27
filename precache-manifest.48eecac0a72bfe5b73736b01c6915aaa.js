42c7177a3477b74014351afe4c5fd4bd5292e1da
Selvakumar Subramaniam <selva-ts@users.noreply.github.com>
2020-04-27 01:15:22 +0100

    Updates

---
+++ precache-manifest.48eecac0a72bfe5b73736b01c6915aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801d3f1c461151c88260816e20c135fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7bd01b4c55af3ca230",
+    "url": "/static/js/2.d8aced0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d8aced0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f326043dd76e02a938ab",
+    "url": "/static/js/main.339201b4.chunk.js"
+  },
+  {
+    "revision": "a1371ee88a8e3e143a94",
+    "url": "/static/js/runtime-main.ddb502d6.js"
+  }
+]);
